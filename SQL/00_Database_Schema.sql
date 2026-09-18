7e42ea247a3a6b28dcbefc4683eebe9059b1f622
vikashGiri27 <vikashgiri70670@gmail.com>
2026-09-18 23:49:50 +0530

    Add Dim_Warehouse table schema

--- SQL/00_Database_Schema.sql
+++ SQL/00_Database_Schema.sql
@@ -13,11 +13,24 @@ Selling_Price decimal(12,2),
 Product_Status varchar(20));
 
 # Import Dim_Product schema :
+
 load data infile 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/Supply_Chain_Cleaned_Data/Dim_Product.csv'
 INTO TABLE Dim_Product
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
+
+/*-----------------------------------------------------------------------------
+                                     DDim_Warehouse Setup
+-------------------------------------------------------------------------------*/
+
+create table Dim_Warehouse
+(Warehouse_ID varchar(10) primary key,
+Warehouse_Name varchar(30),
+Warehouse_Location varchar(40),
+Warehouse_Capacity int);
+
+
 
 
