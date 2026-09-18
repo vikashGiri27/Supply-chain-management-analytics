0c3bf234e0408ba18a723bf7ec2549120d729e9a
vikashGiri27 <vikashgiri70670@gmail.com>
2026-09-19 00:32:54 +0530

    Add data import for Dim_Supplier table

--- SQL/00_Database_Schema.sql
+++ SQL/00_Database_Schema.sql
@@ -12,7 +12,7 @@ Unit_Cost decimal(12,2),
 Selling_Price decimal(12,2),
 Product_Status varchar(20));
 
-# Import Dim_Product schema :
+# Import Dim_Product data :
 
 load data infile 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/Supply_Chain_Cleaned_Data/Dim_Product.csv'
 INTO TABLE Dim_Product
@@ -31,7 +31,7 @@ Warehouse_Name varchar(30),
 Warehouse_Location varchar(40),
 Warehouse_Capacity int);
 
-# Import Dim_Warehouse schema :
+# Import Dim_Warehouse data :
 
 Load Data Infile 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/Supply_Chain_Cleaned_Data/Dim_Warehouse.csv'
 Into Table Dim_Warehouse
@@ -41,7 +41,7 @@ lines terminated by  '\n'
 Ignore 1 rows;
 
 /*-----------------------------------------------------------------------------
-                                     Dim_Warehouse Setup
+                                     Dim_Supplier Setup
 -------------------------------------------------------------------------------*/
 create table Dim_Supplier(	
 Supplier_ID varchar(10) primary key,
@@ -50,8 +50,14 @@ Supplier_Location varchar(20),
 Supplier_Category varchar(40),
 Supplier_Status varchar(30));
 
+# import Dim_Supplier data :
 
-
+Load Data Infile 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/Supply_Chain_Cleaned_Data/Dim_Supplier.csv'
+Into Table Dim_Supplier
+Fields terminated by ','
+Enclosed by '"'
+Lines terminated by '\n'
+Ignore 1 rows;
 
 
 
